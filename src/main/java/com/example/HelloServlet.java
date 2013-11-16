31449c4e7081887e89f5890c3d4b0383c573384a
jwalter <joakim.waltersson@gmail.com>
2013-11-16 21:31:33 +0100

    Initial working version

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,9 +1,10 @@
 package com.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.logging.Logger;
+import java.io.InputStreamReader;
+import java.net.URL;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -16,8 +17,14 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        
-        out.write("Hello Heroku".getBytes());
+        URL oracle = new URL("http://stkorspublicservices.linkoping.se/parkeringsapp/ParkeringsappV1.svc/GetParkeringsytaList/1368176037");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            out.println(inputLine);
+        in.close();
         out.flush();
         out.close();
     }
